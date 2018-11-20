a23cfe6f12bf08cd2a760f68dbd5d55d03435619
Daniel Sims <daniel.sims1994@outlook.com>
2018-11-20 19:41:15 +0000

    Added data provider module

--- app/src/main/java/com/sims/daniel/baseapplication/features/home/HomeViewModel.java
+++ app/src/main/java/com/sims/daniel/baseapplication/features/home/HomeViewModel.java
@@ -2,9 +2,14 @@ package com.sims.daniel.baseapplication.features.home;
 
 import android.arch.lifecycle.ViewModel;
 
+import com.sims.daniel.baseapplication.utils.ProjectApplicationCache;
+
 import javax.inject.Inject;
 
 public class HomeViewModel extends ViewModel {
+
+    @Inject
+    ProjectApplicationCache mProjectApplicationCache;
 
     @Inject
     public HomeViewModel() {
